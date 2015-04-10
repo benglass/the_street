fa4f6004f2997a7bc8fd7921a1f331150e8e6eab
Ben Glassman <ben@vtdesignworks.com>
2015-04-10 18:50:10 -0400

    Convert graph to importable es6 module and load into main component

--- public_html/components/Main.jsx
+++ public_html/components/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Graph from 'Graph';
 
 var MainComponent = React.createClass({
   propTypes: {
@@ -12,6 +13,7 @@ var MainComponent = React.createClass({
   componentDidMount() {
     var node = React.findDOMNode(this);
     node.innerHTML = this.props.message + ' And more!';
+    Graph(node);
   },
   // Never update the dom because this component renders a graph, we update manually
   shouldComponentUpdate(nextProps, nextState) {
