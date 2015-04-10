22d8e4a3fd4ddf6da8a3ab529b59eb170cf96d93
Ben Glassman <ben@vtdesignworks.com>
2015-04-10 18:23:40 -0400

    Add react/jsx compilation with alternate app entry point for react.

---
+++ public_html/app-react.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import Main from './components/Main.jsx!';
+
+React.render(React.createElement(Main), document.getElementById('app'));
---
+++ public_html/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+var MainComponent = React.createClass({
+  render: function() {
+    return (
+      <div>Hi!</div>
+    )
+  }
+});
+
+export default MainComponent;
